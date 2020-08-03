b22a6e17a9b8d61bca09d554d62e7a720db3afdb
Addi P <addipenry@gmail.com>
2020-08-02 20:54:14 -0400

    just need to work on hiding/showing divs! menu now collapses.

--- Website/scripts/helpers.js
+++ Website/scripts/helpers.js
@@ -14,20 +14,30 @@ function loadNavbar() {
 		["education & skills", "../pages/resume.html"]
 		];
 
-	document.write('<nav class="navbar__list navbar__list_left">');
+	document.write('<nav class="navbar navbar-expand-md"><div class="container-fluid"');
+	document.write('<div class="navbar-header">'
+     	// <!--menu hamburger solo per mobile-->
+    	+'<button type="button" class="navbar-toggle d-block d-md-none" data-toggle="collapse" data-target=".navbar-collapse">'
+    	+'<span class="navbar-toggler-icon"></span>'
+    	// +'<span class="icon-bar"></span>'
+     //    +'<span class="icon-bar"></span>'
+     //    +'<span class="icon-bar"></span>'             
+      	+'</button></div>');
+
+	document.write('<div class="collapse navbar-collapse"><ul class="nav navbar-nav navbar__list">')
 
 	for (var i = 0; i < pageRefs.length; i++) {
-		document.write('\n<li><a href="' + pageRefs[i][1] + '">'+pageRefs[i][0]+'</a>');
+		document.write('\n<li class="navbar__list_li navbar__list_li_left"><a href="' + pageRefs[i][1] + '">'+pageRefs[i][0]+'</a>');
 	}
 	// <li><a href="/pages/practice.html">coding practice</a>
-
-	document.write('</nav>');
+	document.write('<li><div class="d-none d-md-block"><img src="../items/navbar_img.png" alt="vertical flowers" width=100%></div></li>');
+	document.write('<li><div class="d-block d-md-none"><img src="../items/navbar_small_img.png" alt="horizontal flowers" width=100%></div></li>');
+	document.write('</ul></div></nav>');
 	// <source media="(min-width:650px)" srcset="img_pink_flowers.jpg">
 	// <source media="(min-width:465px)" srcset="img_white_flower.jpg">
 	// document.write("<picture>");
 	
-	document.write('<div class="d-none d-md-block"><img src="../items/navbar_img.png" alt="vertical flowers" width=100%></div>');
-	document.write('<div class="d-block d-md-none"><img src="../items/navbar_small_img.png" alt="horizontal flowers" width=100%></div>');
+	
 	// document.write("</picture>");
 	// return "</ul>" 
 	// + "<img src=\"../items/navbar_img.png\" id=\"navbar_img\" alt=\"some flowers\"width=100%>" 
@@ -68,7 +78,7 @@ function loadBasicImports(){
 	document.write('<link rel="icon" type="image/png" href="../items/favicon.png"/>');
 	document.write('<meta name="viewport" content="width=device-width, initial-scale=1">');
 	document.write('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">');
-	document.write('<link rel="stylesheet 2" type="text/css" href="../stylesheets/stylesheet.css"/>');
+	document.write('<link rel="my stylesheet" type="text/css" href="../stylesheets/stylesheet.css"/>');
 	// document.write("<script src=\"https://code.jquery.com/jquery-1.10.2.js\"></script>");
 	document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>');
 	document.write('<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>');
