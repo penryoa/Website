2e120b3f516d48da6bfc284dceb6d1609b2b27f6
Addi P <addipenry@gmail.com>
2021-04-20 02:25:57 -0400

    slight refactoring, adding in more styling, updated text

--- src/components/AboutMeContent.js
+++ src/components/AboutMeContent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { SquareTile, ImageTile } from "./Tiles";
 import Img_Skull from "../assets/images/skull_transparent_bg.png";
 import Img_Me from "../assets/images/mother_of_dragon.jpg";
 import Img_Art_Sun from "../assets/images/art_sun-n-moon.jpg";
@@ -28,7 +29,6 @@ const AboutMeContent = () => {
           </div>
         </h2>
         <div className="moon moon-h2" />
-        {/* <img className="img-right" src="../assets/images/me.jpeg" height="100%" width="30%"> */}
       </div>
 
       <div className="chevron">
@@ -49,218 +49,180 @@ const AboutMeContent = () => {
       <div className="py-2">
         <div className="max-w-md m-auto">
           <div className="flex-con flex-center m-auto">
-            <div className="tile__wrap bg-5">
-              <div className="tile__description_layer justified">
-                <h3>music</h3>
+            <SquareTile
+              title="music"
+              content={
                 <p>
-                  Piano has been my favorite instrument since I watched a Mars
-                  documentary at an IMAX theater on a fieldtrip. A cool piano
-                  melody stuck in my head and I've been playing ever since.
+                  Piano is by far my favorite instrument to play. When I was
+                  around 7 or 8, I started playing by ear and never stopped.
                   <br />
-                  I've been singing as long as I can remember as well and have
-                  pretty much always been involved in a choir.
+                  I've been singing as long as I can remember as well and grew
+                  up in different choirs.
                   <br />
-                  Guitar is my newest flame. It's been fun to learn and weird to
-                  build callouses, but so worth it.
+                  My newest instrument is acoustic guitar, which has only been
+                  around a year or so. Definitely not super skilled, but still
+                  enjoying it!
                 </p>
-              </div>
-            </div>
-            <div className="tile__wrap bg-4">
-              <div className="tile__description_layer justified">
-                <h3>languages</h3>
+              }
+              bg_color={5}
+            />
+
+            <SquareTile
+              title="languages"
+              content={
                 <p>
-                  I started learning French about seven years ago and studied
-                  for three years. I'm trying to get back into it and take a
-                  trip to Canada!
+                  I started learning French about eight years ago and studied
+                  for three years. I'd love to get back into it and take a trip
+                  to Canada!
                   <br />
-                  Japanese has been my target language for over a year now,
-                  though with an intense college schedule, time was hard to
-                  find. It's been easier to study now that I'm graduated. My
-                  long-term goal is to get through at least the JLPT5 and JLPT4.
+                  Japanese has been my target language for around two years now,
+                  though time was impossible to find the first year of studying.
+                  My long-term goal is to get through at least the JLPT N5 and
+                  N4, ideally even N3.
                 </p>
-              </div>
-            </div>
-            <div className="tile__wrap bg-6">
-              <div className="tile__description_layer justified">
-                <h3>motorcycles</h3>
+              }
+              bg_color={4}
+            />
+
+            <SquareTile
+              title="motorcycles"
+              content={
                 <p>
-                  Roxas, my bike, is a 2007 Honda Shadow. I've had him for two
-                  years. In that time, I've taken plenty of day trips and
-                  visited places even four hours away. Highly recommend it (if
-                  you take safety seriously)!
+                  Roxas, my bike, is a 2007 Honda Shadow. I've had him for three
+                  years. In that time, I've taken plenty of day trips and even
+                  some that require four or five hours on the road at a time.
+                  Highly recommend it.
                   <br />
                   At one point, I had to take my dad on the back of my bike in
                   the pouring rain at night. He had a full gas canister in his
                   lap. I prefer more leisurely rides than that.
                 </p>
-              </div>
-            </div>
+              }
+              bg_color={6}
+            />
+
             <div className="tile__wrap">
               <img src={Img_Skull} alt="skull" height="100%" />
             </div>
-            <div className="tile__wrap bg-7">
-              <div className="tile__description_layer justified">
-                <h3>creativity</h3>
+
+            <SquareTile
+              title="creativity"
+              content={
                 <p>
                   My left and right brain are both hyperactive most of the time
                   and creative things tend to be the best way to calm either
                   side. I really like playing with tons of different mediums -
-                  poems, writing songs, painting, drawing, sculpting,
-                  papercraft, and even interior design.
-                  <br />
-                  Basically, I'm never bored and I highly recommend finding
-                  creative outlets! It's a game changer if you find yourself
-                  stressed or disconnected from yourself.
+                  poems, writing songs, painting, drawing, papercraft, coding,
+                  world building, and even interior design.
                 </p>
-              </div>
-            </div>
-            <div className="tile__wrap bg-1">
-              <div className="tile__description_layer justified">
-                <h3>video games</h3>
+              }
+              bg_color={7}
+            />
+
+            <SquareTile
+              title="video games"
+              content={
                 <p>
                   I'm a sucker for Nintendo games. Growing up with N64 and Super
                   Nintendo led to a pretty inevitable obsession with Zelda games
-                  especially. Animal Crossing will always be a soft spot for me.
-                  <br />
-                  Outside of Nintendo, my games of choice are predictable:
-                  Skyrim, Fallout, and dabbling otherwise. I've watched many
-                  more games be played than I've played myself honestly, the
-                  best ones being Fran Bow and other horror/spooky games.
+                  especially. Outside of Nintendo, my games of choice are
+                  simple: Skyrim, Fallout, Kingdom Hearts, and dabbling
+                  otherwise. I've watched many more games be played than I've
+                  played myself honestly, the best ones being Fran Bow and other
+                  horror/spooky games.
                 </p>
-              </div>
-            </div>
+              }
+              bg_color={1}
+            />
           </div>
         </div>
       </div>
 
-      <div className="bg-5 rounded-md">
+      <div className="cool_bg dotted-border rounded-md">
         <div className="p-1 text-centered">
           <p>Enjoy some random pictures of things that make me smile (:</p>
         </div>
         <div className="flex-con flex-center">
           {/* ROW 1 */}
-          <div className="img__wrap">
-            <img src={Img_Me} alt="just me" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description">
-                Obligatory selfie ft. Dragon perched on my shoulder
-              </p>
-            </div>
-          </div>
-          <div className="img__wrap">
-            <img src={Img_Art_Sun} alt="art - sun and moon" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description">A painting of mine from 2015.</p>
-            </div>
-          </div>
-          <div className="img__wrap">
-            <img src={Img_Mykie} alt="my lil lady Mykie" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description">
-                This is Mykie, aka Mykie Lady. Her hobbies include lounging
-                outdoors or by windows. She only lets you know you're giving her
-                too much attention when she is completely annoyed.
-              </p>
-            </div>
-          </div>
-          <div className="img__wrap">
-            <img src={Img_Dragon} alt="my lil boyo Dragon" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description justified">
-                This is Dragon, aka Mr. Dragon. He's a strange guy, sometimes
-                even creepy. His biggest fear is starvation by an empty food
-                bowl.
-              </p>
-            </div>
-          </div>
+          <ImageTile
+            img_src={Img_Me}
+            alt_text="just me"
+            desc_text="Obligatory selfie ft. Dragon perched on my shoulder"
+          />
+          <ImageTile
+            img_src={Img_Art_Sun}
+            alt_text="art - sun and moon"
+            desc_text="A painting of mine from 2015."
+          />
+          <ImageTile
+            img_src={Img_Mykie}
+            alt_text="my lil lady Mykie"
+            desc_text="This is Mykie, aka Mykie Lady. Her hobbies include lounging
+            outdoors or by windows. She only lets you know you're giving her
+            too much attention when she is completely annoyed."
+          />
+          <ImageTile
+            img_src={Img_Dragon}
+            alt_text="my lil boyo Dragon"
+            desc_text="This is Dragon, aka Mr. Dragon. He's a strange guy, sometimes
+            even creepy. His biggest fear is starvation by an empty food
+            bowl."
+          />
 
           {/* ROW 2 */}
-          <div className="img__wrap">
-            <img src={Img_Bridges} alt="bridges in NYC" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description">
-                Taken from one of the many scenic waterviews in Manhattan.
-              </p>
-            </div>
-          </div>
-          <div className="img__wrap">
-            <img
-              src={Img_Roxas}
-              alt="my motorcycle Roxas and I"
-              height="100%"
-            />
-            <div className="img__description_layer">
-              <p className="img__description">
-                My boy Roxas. He's a stud with that glistening pearl coat.
-              </p>
-            </div>
-          </div>
-          <div className="img__wrap">
-            <img src={Img_Mushrooms} alt="mushrooms" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description">
-                Very aesthetically pleasing fly amanita mushrooms in North
-                Carolina
-              </p>
-            </div>
-          </div>
+          <ImageTile
+            img_src={Img_Bridges}
+            alt_text="bridges in NYC"
+            desc_text="Taken from one of the many scenic waterviews in Manhattan."
+          />
+          <ImageTile
+            img_src={Img_Roxas}
+            alt_text="my motorcycle Roxas and I"
+            desc_text="My boy Roxas. He's a stud with that glistening pearl coat."
+          />
+          <ImageTile
+            img_src={Img_Mushrooms}
+            alt_text="mushrooms"
+            desc_text="Very aesthetically pleasing fly amanita mushrooms in North
+        Carolina"
+          />
 
           {/* ROW 3 */}
-          <div className="img__wrap">
-            <img src={Img_Cloudy} alt="cloudy NYC" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description">Just a cloudy day in NYC.</p>
-            </div>
-          </div>
-          <div className="img__wrap">
-            <img src={Img_Bird_Bryant} alt="bird in the park" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description">
-                Pigeons are very fun to be around (:
-              </p>
-            </div>
-          </div>
-          <div className="img__wrap">
-            <img src={Img_Mykie_Sink} alt="mykie in the sink" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description">
-                Sometimes you just want to <b>sink</b> into a deep sleep,
-                amiright?
-              </p>
-            </div>
-          </div>
+          <ImageTile
+            img_src={Img_Cloudy}
+            alt_text="cloudy NYC"
+            desc_text="Just a cloudy day in NYC."
+          />
+          <ImageTile
+            img_src={Img_Bird_Bryant}
+            alt_text="bird in the park"
+            desc_text="Pigeons are very fun to be around (:"
+          />
+          <ImageTile
+            img_src={Img_Mykie_Sink}
+            alt_text="mykie in the sink"
+            desc_text="Sometimes you just want to ~sink~ into a deep sleep,
+        amiright?"
+          />
 
           {/* ROW 4 */}
-          <div className="img__wrap">
-            <img src={Img_Bird_Foot} alt="bird on my foot" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description">
-                I can't get over this one - a bird just decided to hang out on
-                my foot while I read! SO CUTE.
-              </p>
-            </div>
-          </div>
-          <div className="img__wrap">
-            <img
-              src={Img_South_Cove}
-              alt="yet another water photo"
-              height="100%"
-            />
-            <div className="img__description_layer">
-              <p className="img__description">
-                Stumbled upon a really neat park in NYC!
-              </p>
-            </div>
-          </div>
-          <div className="img__wrap">
-            <img src={Img_Art_Hand} alt="art - hand of time" height="100%" />
-            <div className="img__description_layer">
-              <p className="img__description">
-                A painting of mine from 2017. Kind of makes me think of the
-                "hand of time" in a weird way.
-              </p>
-            </div>
-          </div>
+          <ImageTile
+            img_src={Img_Bird_Foot}
+            alt_text="bird on my foot"
+            desc_text="I can't get over this one - a bird just decided to hang out on
+        my foot while I read! SO CUTE."
+          />
+          <ImageTile
+            img_src={Img_South_Cove}
+            alt_text="south cove park"
+            desc_text="Stumbled upon a really neat park in NYC!"
+          />
+          <ImageTile
+            img_src={Img_Art_Hand}
+            alt_text="art - hand of time"
+            desc_text='A painting of mine from 2017. Kind of makes me think of the
+        "hand of time" in a weird way.'
+          />
         </div>
       </div>
     </>
---
+++ src/components/Tiles.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function SquareTile({ title, content, bg_color = 1 }) {
+  return (
+    <div className={"dotted-border tile__wrap bg-" + bg_color}>
+      <div className="tile__description_layer justified">
+        <h3>{title}</h3>
+        {content}
+      </div>
+    </div>
+  );
+}
+
+function ImageTile({ img_src, alt_text, desc_text }) {
+  return (
+    <div className="img__wrap">
+      <img src={img_src} alt={alt_text} height="100%" />
+      <div className="img__description_layer">
+        <p className="img__description justified">{desc_text}</p>
+      </div>
+    </div>
+  );
+}
+
+export { SquareTile, ImageTile };
