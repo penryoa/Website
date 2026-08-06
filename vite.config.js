326d365695293e4633f75fead5e88e8de7cdb674
penryoa <addipenry@gmail.com>
2026-08-06 19:55:00 -0400

    update vite config to include tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [tailwindcss(), react(), cloudflare()],
 })
